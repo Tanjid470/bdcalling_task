8f6bee60c0dab62ec6135530bfd50ac551585cb7
Tanjid470 <tanjidamran11@gmail.com>
2025-01-09 22:08:01 +0600

    local cache file

---
+++ lib/modules/assignment_three/model/data_fetch_model.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_fetch_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+
+class DataModelAdapter extends TypeAdapter<DataModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  DataModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return DataModel(
+      userId: fields[0] as int,
+      id: fields[1] as int,
+      title: fields[2] as String,
+      completed: fields[3] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, DataModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.userId)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.completed);
+  }
+}
